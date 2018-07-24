156e06149086b167157487ec404e4c72479ef415
Mara <levymaratx@gmail.com>
2018-07-24 15:21:02 -0500

    pu

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var randomMovieArray = ['Star Wars', 'Game of Thrones', 'Jaws'];
 function apiCall(){
   var randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
   var randomMovie = randomMovieArray[randomNumber];
-console.log(randomMovie);
   $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&h=600&apikey=454a6e93').then(function(response){
   var image = response.Poster;
-console.log(image);
   if(image !== "N/A"){
     $('img').attr('src', image);
     
